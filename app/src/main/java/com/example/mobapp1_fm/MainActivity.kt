b70df2a8b5bf36db32d1433580e6446039689bf8
fionnmoran <fionnmoran13@gmail.com>
2022-08-10 23:57:06 +0100

    Signup/Signin with firebase complete + Theme v1

--- app/src/main/java/com/example/mobapp1_fm/MainActivity.kt
+++ app/src/main/java/com/example/mobapp1_fm/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.mobapp1_fm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.mobapp1_fm.activities.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private var auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflateMenu = menuInflater
+        inflateMenu.inflate(R.menu.logoutmenu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logout) {
+            auth.signOut()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
